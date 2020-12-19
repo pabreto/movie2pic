b7a64ac14db9f9c597c96371823dcb9077695721
pabreto <pabretonniere@hotmail.fr>
2020-12-19 23:53:17 +0100

    Working code

--- movie2pic.py
+++ movie2pic.py
@@ -4,6 +4,8 @@ import cv2
 from PIL import Image
 from configparser import ConfigParser
 import os
+from collections import Counter
+import webcolors
 import numpy as np
 
 config_file: ConfigParser = ConfigParser()
@@ -24,35 +26,67 @@ currentframe = 0
 # create empty final black pic with 4 horizontal pixels per frame, height defined in config file
 cam = cv2.VideoCapture(movie_name)
 number_of_frames = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
-print(number_of_frames)
-print(movie_name)
-if os.path.isfile(movie_name):
-    print('ok')
+
+if not os.path.isfile(movie_name):
+    print("Movie " + movie_name + " doesn't exist")
 else:
-    print('no')
+    print('Reading ', movie_name)
+    print('Number of frames: ', number_of_frames)
+
 if not os.path.exists(outdir):
     os.makedirs(outdir)
 
+# A = np.array(
+#  [[ 2.,  1.],
+#   [ 1.,  1.]]
+# )
+
+# c = Counter(map(tuple, A)).most_common()[0][0]
+# print("c",c)
 final_pic = Image.new('RGB', (number_of_frames * x_frame, height), "black")
 while currentframe < number_of_frames:
     ret, frame = cam.read()
     if ret:
-        print(frame.shape)
-        for i in range(0, 4):
-            print("i", frame[i, 0, 0])
-        cv2.imshow('frame', frame)
-        cv2.waitKey(0)
+        # print(frame.shape)
+        x_orig = frame[:, 0, 0].size
+        h_orig = frame[0, :, 0].size
+        print('x_orig', x_orig)
+        print('h_orig', h_orig)
+        #        print(frame[0,0,0])
+        #        print(webcolors.rgb_to_name((0, 0, 0), spec='css3'))
+        #        print(webcolors.rgb_to_name(list(frame[0,0,0]),spec='css3'))
+        #        for i in range(0, 3):
+        #            print("i", frame[0, :, i])
+        cv2.imshow('frame' + str(currentframe), frame)
+        cv2.waitKey()
+        #        print("newframe",newframe)
+        #        print("frame",frame)
+        print('separations', separations)
+        for h in range(0, separations):
+            new_height = int(height / separations)
+            slice_hsize = int(h_orig / separations)
+            slice_size = int(x_orig * slice_hsize)
+            print('height', height)
+            print('new_height', new_height)
+            print('slice_hsize', slice_hsize)
+            print('slice_size', slice_size)
+            cv2.imshow('subframe' + str(currentframe), frame[:, int(h * slice_hsize):int((h + 1) * slice_hsize), :])
+            subframe = frame[:, int(h * slice_hsize):int((h + 1) * slice_hsize), :].reshape(slice_size, 3)
+            mostfrequent_color = Counter(map(tuple, subframe[h:int((h + 1) * slice_hsize)])).most_common()[0][0]
+            print('subframe', subframe)
+            print('mostfrequent_color', mostfrequent_color)
+            x1 = currentframe * x_frame
+            y1 = h * new_height
+            x2 = (currentframe + 1) * x_frame
+            y2 = (h + 1) * new_height
+            final_pic.paste(mostfrequent_color,
+                            (x1, y1, x2, y2))
 
-
-        tmppicname = os.path.join(outdir, pic_name + "_" + str(currentframe) + '.jpg')
-        print('Creating...' + tmppicname)
-        #        frame =
-        #        color = # array ( (RGB), separations)
-        #        for i in range(0,separations):
-        #            final_pic.paste(color[i],(currentframe*x_frame,i*height,(currentframe+1)*x_frame,(i+1)*height))
-        #            final_pic.show()
+            # tmppic = Image.new('RGB', (x_frame, new_height), mostfrequent_color)
+            cv2.waitKey()
         currentframe += 1
-
+        final_pic.show()
+final_pic.save(pic_name + ".jpg")
 # writing the extracted images
 # increasing counter so that it will 
 # show how many frames are created 
--- movie_generator.py
+++ movie_generator.py
@@ -5,6 +5,7 @@ from configparser import ConfigParser
 
 import cv2
 import numpy as np
+import webcolors
 from PIL import Image
 
 config_file = ConfigParser()
@@ -14,15 +15,14 @@ config_file.read('options-generator.conf')
 number_of_frames = int(config_file.get("DEFAULT", "number_of_frames"))
 height = int(config_file.get("DEFAULT", "height"))
 x_frame = int(config_file.get("DEFAULT", "x_frame"))
-separation_vert = int(config_file.get("DEFAULT", "separation_vert"))
-separation_horiz = int(config_file.get("DEFAULT", "separation_horiz"))
+number_of_rows = int(config_file.get("DEFAULT", "number_of_rows"))
+number_of_columns = int(config_file.get("DEFAULT", "number_of_columns"))
 video_name = config_file.get("DEFAULT", "video_name")
 c1p1 = config_file.get("DEFAULT", "c1p1")
 c2p1 = config_file.get("DEFAULT", "c2p1")
 c1p2 = config_file.get("DEFAULT", "c1p2")
 c2p2 = config_file.get("DEFAULT", "c2p2")
 
-final_pic = Image.new('RGB', (number_of_frames * x_frame, height), "pink")
 
 red = [255, 0, 0]
 white = [255, 255, 255]
@@ -32,17 +32,8 @@ yellow = [255, 255, 0]
 green = [0, 255, 0]
 purple = [153, 0, 153]
 orange = [255, 128, 0]
-color2rgb = {
-    "red": [255, 0, 0, 100, 100],
-    "white": [255, 255, 255],
-    "blue": [51, 51, 255],
-    "black": [51, 0, 0],
-    "yellow": [255, 255, 0],
-    "green": [0, 255, 0],
-    "purple": [153, 0, 153],
-    "orange": [255, 128, 0]
-}
 
+#final_pic = Image.new('RGB', (number_of_frames * x_frame, height), tuple(yellow))
 # print(color2rgb["red"])
 # print( np.array(c1p1))
 # print(color2rgb(np.array(c1p1)))
@@ -55,25 +46,28 @@ color2rgb = {
 #        [ np.array(c1p2),\
 #        np.array(c2p2)]])
 # print("shape",color.shape)
-color = np.array([[[red, red, blue, black],
+color = np.array([[[red, purple, blue, black],
                    [red, green, black, blue]],
                   [[yellow, green, purple, orange],
                    [red, white, blue, black]]])
-color = np.array([[[red, red, red, red],
-                   [red, red, red, red]],
-                  [[green, green, green, green],
-                   [green, green, green, blue]]])
+#color = np.array([[[red, black, red, white],
+#                   [red, black, red, orange]],
+#                  [[purple, purple, purple, green],
+#                   [green, green, green, blue]]])
+
 
 for currentframe in range(0, number_of_frames):
-    final_pic = Image.new('RGB', (number_of_frames * x_frame, height), "red")
-    for horiz in range(0, separation_horiz):
-        for vertic in range(0, separation_vert):
+    final_pic = Image.new('RGB', (number_of_frames * x_frame, height), tuple(blue))
+    for horiz in range(0, number_of_columns):
+        for vertic in range(0, number_of_rows):
             x1 = horiz * x_frame
-            y1 = vertic * int((height / separation_vert))
+            y1 = vertic * int((height / number_of_rows))
             x2 = (horiz + 1) * x_frame
-            y2 = (vertic + 1) * int((height / separation_vert))
-            #print(y2-y1+1)
+            y2 = (vertic + 1) * int((height / number_of_rows))
+#            print(y2-y1+1)
+#            final_pic.paste(tuple(color[currentframe, horiz, vertic, :]), (x1, y1, x2, y2))
             final_pic.paste(tuple(color[currentframe, horiz, vertic, :]), (x1, y1, x2, y2))
+
     final_pic.show()
     print(final_pic.mode)
     currentframe += 1
